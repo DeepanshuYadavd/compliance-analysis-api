04bae49197da6f68b6012394f8a9d1ee5a0ad9af
DeepanshuYadavd <dipanshuy591@gmail.com>
2026-04-16 12:52:01 +0530

    updated

---
+++ api/models/framework.model.js
@@ -0,0 +1,65 @@
+import mongoose, { Mongoose } from "mongoose";
+import { randomUUID } from "crypto";
+
+const frameworkSchema = new mongoose.Schema(
+  {
+    uuid: {
+      type: String,
+      default: () => randomUUID(),
+      unique: true,
+      index: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    shortCode: {
+      type: String,
+      required: true,
+      unique: true,
+      upperCase: true,
+    },
+    description: String,
+    authority: {
+      type: String,
+    },
+    country: String,
+    appliesTo: {
+      type: [String],
+      enum: ["company", "product"],
+    },
+    industry: String,
+    controls: [
+      {
+        controlId: String,
+        title: String,
+        description: String,
+        requirementText: String,
+        mandatory: {
+          type: Boolean,
+          deafult: true,
+        },
+
+        riskLevel: {
+          type: String,
+          enum: ["low", "medium", "high"],
+          default: "medium",
+        },
+        tags: [String],
+      },
+    ],
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Auth",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+export const Framework = mongoose.model("Framework", frameworkSchema);
